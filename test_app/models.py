38b21b25e61f9ec833e383c2c82d4e686c8174aa
HordiienP47 <gordienkoth@gmail.com>
2025-11-14 18:56:05 +0100

    Added models

--- test_app/models.py
+++ test_app/models.py
@@ -1,3 +1,19 @@
 from django.db import models
 
-# Create your models here.
+from model import Models
+
+class Task(models.Model):
+    title = models.CharField(max_length=120)
+    description = models.TextField()
+    categories =
+    status =
+    deadline =
+    created_at =
+
+class SubTask(models.Model):
+    title =
+    description =
+    task =
+    status =
+    deadline =
+    created_at =
